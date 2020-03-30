8f11d52fa91f832acca018ced27947cda6341164
Martin-SW-SW <62882502+Martin-SW-SW@users.noreply.github.com>
2020-03-30 13:25:05 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Flinter
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Profile profile1 = new Profile(true, Flinter.Profile.eyeColor.Blue, Flinter.Profile.hairColor.blond, 1);
+            Profile profile2 = new Profile(false, Flinter.Profile.eyeColor.Brown, Flinter.Profile.hairColor.white, 4);
+            Profile profile3 = new Profile(true, 0, 0, 8);
+
+            Console.WriteLine(profile1.Description);
+            Console.WriteLine(profile2.Description);
+            Console.WriteLine(profile3.Description);
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
